60f7954bc9d9cfa50cbfb8c87da2e42f1725905d
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2022-03-20 11:45:58 -0500

    Add proper server defaults

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,5 @@
 use crate::{client::Client, counter::Counter, packet::Packet};
 
-#[derive(Default)]
 pub struct Server {
     access_key: String,
     prudp_version: u32,
@@ -15,6 +14,26 @@ pub struct Server {
     kerberos_key_derivation: u32,
     server_version: u32,
     connection_id_counter: Counter,
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        Self {
+            access_key: "".to_string(),
+            nex_version: 0,
+            server_version: 0,
+            use_packet_compression: false,
+            prudp_version: 1,
+            fragment_size: 1300,
+            ping_timeout: 5,
+            signature_version: 0,
+            flags_version: 1,
+            checksum_version: 1,
+            kerberos_key_size: 32,
+            kerberos_key_derivation: 0,
+            connection_id_counter: Counter::default(),
+        }
+    }
 }
 
 impl Server {

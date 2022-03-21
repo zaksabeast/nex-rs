32ff9bbe929ce7e58de2a9ee0b7047fcdceaf126
Steve Cook <steviecook210@gmail.com>
2022-03-21 11:36:02 -0400

    Implement methods
    Client::reset
    Client::update_rc4_key
    Client::update_access_key

--- src/client.rs
+++ src/client.rs
@@ -5,11 +5,14 @@ use crate::{
     server::Server,
 };
 use std::net::SocketAddr;
+use md5::{Md5, Digest};
 
 pub struct ClientContext {
+    pub access_key: String,
     pub cipher: Rc4,
     pub decipher: Rc4,
     pub flags_version: u32,
+    pub prudp_version: u32,
     pub signature_key: Vec<u8>,
     pub signature_base: u32,
     pub session_key: Vec<u8>,
@@ -18,9 +21,11 @@ pub struct ClientContext {
 impl Default for ClientContext {
     fn default() -> Self {
         Self {
+            access_key: String::new(),
             cipher: Rc4::new(&[0]),
             decipher: Rc4::new(&[0]),
             flags_version: 1,
+            prudp_version: 1,
             signature_key: vec![],
             signature_base: 0,
             session_key: vec![],
@@ -58,9 +63,11 @@ impl Client {
             sequence_id_in: Counter::default(),
             sequence_id_out: Counter::default(),
             context: ClientContext {
+                access_key: server.get_access_key(),
                 cipher: Rc4::new(&[0]),
                 decipher: Rc4::new(&[0]),
                 flags_version: server.get_flags_version(),
+                prudp_version: server.get_prudp_version(),
                 signature_key: vec![],
                 signature_base: 0,
                 session_key: vec![],
@@ -80,12 +87,30 @@ impl Client {
         self.client_connection_signature = client_connection_signature;
     }
 
+    pub fn set_server_connection_signature(&mut self, server_connection_signature: Vec<u8>) {
+        self.server_connection_signature = server_connection_signature;
+    }
+
     pub fn set_is_connected(&mut self, is_connected: bool) {
         self.is_connected = is_connected;
     }
 
     pub fn reset(&mut self) {
-        unimplemented!();
+        self.sequence_id_in = Counter::default();
+        self.sequence_id_out = Counter::default();
+
+        self.update_access_key(self.context.access_key.to_string());
+        self.update_rc4_key("CD&ML".as_bytes().to_vec());
+
+        if self.context.prudp_version == 0 {
+            self.set_client_connection_signature(vec![0; 4]);
+            self.set_server_connection_signature(vec![0; 4]);
+        } else {
+            self.set_client_connection_signature(vec![]);
+            self.set_server_connection_signature(vec![]);
+        }
+
+        self.set_is_connected(false);
     }
 
     pub fn get_address(&self) -> SocketAddr {
@@ -105,11 +130,17 @@ impl Client {
     }
 
     fn update_rc4_key(&mut self, rc4_key: Vec<u8>) {
-        unimplemented!();
+        self.context.cipher = Rc4::new(&rc4_key.clone());
+        self.context.decipher = Rc4::new(&rc4_key.clone());
     }
 
     fn update_access_key(&mut self, access_key: String) {
-        unimplemented!();
+        self.context.signature_base = access_key.as_bytes().iter().map(|&i| i as u32).sum();
+        let mut md5 = Md5::new();
+
+        md5.update(access_key.as_bytes());
+
+        self.context.signature_key = md5.finalize().to_vec();
     }
 
     pub fn increase_ping_timeout_time(&mut self, seconds: u32) {

b646995c5da092c9b768acec2c52101ccd05ffbf
Steve Cook <steviecook210@gmail.com>
2022-03-23 16:13:27 -0400

    Change decompress to result

--- src/compression.rs
+++ src/compression.rs
@@ -10,13 +10,13 @@ pub mod dummy_compression {
 
 pub mod zlib_compression {
     use miniz_oxide::deflate::compress_to_vec_zlib;
-    use miniz_oxide::inflate::decompress_to_vec_zlib;
+    use miniz_oxide::inflate::{decompress_to_vec_zlib, TINFLStatus};
 
     pub fn compress(data: &[u8]) -> Vec<u8> {
         compress_to_vec_zlib(data, 6)
     }
 
-    pub fn decompress(data: &[u8]) -> Vec<u8> {
-        decompress_to_vec_zlib(data).expect("Failed to decompress")
+    pub fn decompress(data: &[u8]) -> Result<Vec<u8>, TINFLStatus> {
+        Ok(decompress_to_vec_zlib(data)?)
     }
 }

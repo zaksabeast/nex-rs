88f7f39a0e1e0f3f04997a1d98d8ff8bd35e9362
zaksabeast <19464008+zaksabeast@users.noreply.github.com>
2022-03-29 21:25:50 -0500

    Implement EndianWrite for ResultCode

--- src/nex_types/result_code.rs
+++ src/nex_types/result_code.rs
@@ -1,4 +1,5 @@
-use no_std_io::{EndianRead, Error, ReadOutput};
+use core::mem;
+use no_std_io::{EndianRead, EndianWrite, Error, ReadOutput, Writer};
 
 #[derive(Default)]
 pub struct ResultCode(u32);
@@ -17,5 +18,26 @@ impl EndianRead for ResultCode {
 impl From<u32> for ResultCode {
     fn from(result_code: u32) -> Self {
         Self(result_code)
+    }
+}
+
+impl From<ResultCode> for u32 {
+    fn from(result_code: ResultCode) -> Self {
+        result_code.0
+    }
+}
+
+impl EndianWrite for ResultCode {
+    fn get_size(&self) -> usize {
+        mem::size_of::<u32>()
+    }
+
+    fn try_write_le(&self, mut dst: &mut [u8]) -> Result<usize, Error> {
+        dst.write_le(0, &self.0)?;
+        Ok(self.get_size())
+    }
+
+    fn try_write_be(&self, dst: &mut [u8]) -> Result<usize, Error> {
+        unimplemented!()
     }
 }
